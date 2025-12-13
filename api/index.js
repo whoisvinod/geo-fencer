6ab477efb804fbd9a1883351df7cdedcd0d2de43
Vinod Akula <joylumestudios@gmail.com>
2025-12-13 23:47:34 +0530

    Debug Vercel API errors

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../dist')));
+
 
 
 // Database Setup (Turso)
